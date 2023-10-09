7d0469969583d11bd2190bce91cc83e901dd14ed
Lzzzzy4 <lzy1522659928@mail.ustc.edu.cn>
2023-10-09 16:54:15 +0800

    111

--- software/functest/build/riscv32/src/movsx.d
+++
@@ -1,6 +0,0 @@
-/home/user/cpu/CECS-Lab/software/functest/build/riscv32/src/movsx.o: \
- /home/user/cpu/CECS-Lab/software/functest/src/movsx.c \
- /home/user/cpu/CECS-Lab/software/functest/include/trap.h \
- ../base-port/base/include/base.h ../base-port/base/include/arch.h \
- ../base-port/base/include/dev.h ../base-port/base/include/base-macro.h \
- ../base-port/tool/include/tool.h
--- software/functest/build/riscv32/src/select-sort.d
+++
@@ -1,6 +0,0 @@
-/home/user/cpu/CECS-Lab/software/functest/build/riscv32/src/select-sort.o: \
- /home/user/cpu/CECS-Lab/software/functest/src/select-sort.c \
- /home/user/cpu/CECS-Lab/software/functest/include/trap.h \
- ../base-port/base/include/base.h ../base-port/base/include/arch.h \
- ../base-port/base/include/dev.h ../base-port/base/include/base-macro.h \
- ../base-port/tool/include/tool.h
