d86af23cbc66a52c42677b70e3b7486f6f694c69
Lzzzzy4 <lzy1522659928@mail.ustc.edu.cn>
2023-10-10 11:42:55 +0800

    hazard

--- simulator/IP/mycpu/Hazard.sv
+++ simulator/IP/mycpu/Hazard.sv
@@ -72,7 +72,14 @@ module Hazard(
     wire is_load_ex = mem_access_ex[`LOAD_BIT];
     always_comb begin
         // Lab3 TODO: generate stall_by_load_use and flush_by_load_use
-        
+        if(is_load_ex && (rf_rd_ex == rf_rs1_id || rf_rd_ex == rf_rs2_id))begin
+            stall_by_load_use = 1'b1;
+            flush_by_load_use = 1'b1;
+        end
+        else begin
+            stall_by_load_use = 1'b0;
+            flush_by_load_use = 1'b0;
+        end
     end
 
     // control hazard
@@ -81,20 +88,20 @@ module Hazard(
     // Lab4 TODO: generate ecall and mret flush signal
 
     // Lab3 TODO: generate pc_set, IF1_IF2_flush, IF2_ID_flush, ID_EX_flush, EX_LS_flush, LS_WB_flush
-    // assign pc_set           = 
-    // assign IF1_IF2_flush    = 
-    // assign IF2_ID_flush     = 
-    // assign ID_EX_flush      = 
-    // assign EX_LS_flush      = 
-    // assign LS_WB_flush      = 
+    assign pc_set           = jump;
+    assign IF1_IF2_flush    = flush_by_jump;
+    assign IF2_ID_flush     = flush_by_jump;
+    assign ID_EX_flush      = flush_by_jump | flush_by_load_use;
+    assign EX_LS_flush      = 0;
+    assign LS_WB_flush      = 0;
 
     // Lab3 TODO: generate pc_stall, IF1_IF2_stall, IF2_ID_stall, ID_EX_stall, EX_LS_stall, LS_WB_stall
-    // assign pc_stall         = 
-    // assign IF1_IF2_stall    = 
-    // assign IF2_ID_stall     = 
-    // assign ID_EX_stall      = 0;
-    // assign EX_LS_stall      = 0;
-    // assign LS_WB_stall      = 0;
+    assign pc_stall         = stall_by_load_use;
+    assign IF1_IF2_stall    = stall_by_load_use;
+    assign IF2_ID_stall     = stall_by_load_use;
+    assign ID_EX_stall      = 0;
+    assign EX_LS_stall      = 0;
+    assign LS_WB_stall      = 0;
 
     always_comb begin
         pc_set_target = jump_target;
