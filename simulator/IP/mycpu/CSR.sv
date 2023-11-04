bf6130f88beb2fb5d5021f6022d301bf1ff140d4
Lzzzzy4 <lzy1522659928@mail.ustc.edu.cn>
2023-11-04 21:57:33 +0800

    start lab5

--- simulator/IP/mycpu/CSR.sv
+++ simulator/IP/mycpu/CSR.sv
@@ -1,3 +1,4 @@
+`timescale 1ns/1ps
 `include "./include/config.sv"
 module CSR(
     input  logic [ 0:0] clk,
@@ -7,32 +8,27 @@ module CSR(
     input  logic [ 0:0] we,
     input  logic [31:0] wdata,
     output logic [31:0] rdata,
+
     output logic [31:0] mepc_out,
     input  logic [31:0] pc_wb,
-    output logic [31:0] mtvec_out,
-    input  logic [31:0] mcause_in,
-    input  logic [ 4:0] priv_vec_wb
-    // Lab4 TODO: you need to add some input or output pors to implement CSRs' special functions
-);
-    import "DPI-C" function void set_csr_ptr(input logic [31:0] m1 [], input logic [31:0] m2 [], input logic [31:0] m3 [], input logic [31:0] m4 []);
-    logic [31:0] mstatus;
-    logic [31:0] mtvec;
-    logic [31:0] mcause;
-    logic [31:0] mepc;
-    initial begin
-        set_csr_ptr(mstatus, mtvec, mepc, mcause);
-        mstatus = 0;
-        mtvec   = 0;
-        mcause  = 0;
-        mepc    = 0;
-    end
 
-    
+    output logic [31:0] mtvec_out,
+
+    input  logic [31:0] mcause_in,
+
+    input  logic [ 4:0] priv_vec_wb
+);
+
+`ifdef DIFF
+    import "DPI-C" function void set_csr_ptr(input logic [31:0] m1 [], input logic [31:0] m2 [], input  logic [31:0] m3 [], input  logic [31:0] m4 []);
+`endif
+    wire has_exp = |mcause_in;
+    reg [31:0] mstatus;
     always_ff @(posedge clk) begin
         if(!rstn) begin
             mstatus <= 32'h0;
         end
-        else if(|mcause_in) begin
+        else if(has_exp) begin
             mstatus <= {mstatus[31:12], mstatus[8:0], 3'h6};
         end
         else if(priv_vec_wb[`MRET]) begin
@@ -43,7 +39,7 @@ module CSR(
         end
     end
 
-    
+    reg [31:0] mtvec;
     assign mtvec_out = mtvec;
     always_ff @(posedge clk) begin
         if(!rstn) begin
@@ -54,12 +50,12 @@ module CSR(
         end
     end
 
-    
+    reg [31:0] mcause;
     always_ff @(posedge clk) begin
         if(!rstn) begin
             mcause <= 32'h0;
         end
-        else if(|mcause_in) begin
+        else if(has_exp) begin
             mcause <= mcause_in;
         end
         else if(waddr == `CSR_MCAUSE && we) begin
@@ -67,13 +63,13 @@ module CSR(
         end
     end
 
-    
+    reg [31:0] mepc;
     assign mepc_out = mepc;
     always_ff @(posedge clk) begin
         if(!rstn) begin
             mepc <= 32'h0;
         end
-        else if(|mcause_in) begin
+        else if(has_exp) begin
             mepc <= pc_wb;
         end
         else if(waddr == `CSR_MEPC && we) begin
@@ -81,6 +77,7 @@ module CSR(
         end
     end
 
+    // read
     always_comb begin
         case(raddr)
             `CSR_MSTATUS: rdata = mstatus;
@@ -90,5 +87,9 @@ module CSR(
             default     : rdata = 32'h0;
         endcase
     end
-    
+    initial begin
+`ifdef DIFF
+        set_csr_ptr(mstatus, mtvec, mepc, mcause);
+`endif
+    end
 endmodule
--- simulator/IP/mycpu/Exp_Commit.sv
+++ simulator/IP/mycpu/Exp_Commit.sv
@@ -1,7 +1,13 @@
+`timescale 1ns/1ps
 `include "./include/config.sv"
-
-// Lab4 TODO: implement the Exp_Commit module for ecall instruction
-// you need to generate exception code for `syscall from machine mode`
 module Exp_Commit(
+    input  logic [ 4:0] priv_vec,
+    output logic [31:0] exp_code
 );
+    always_comb begin
+        exp_code = 0;
+        if(priv_vec[`ECALL]) begin
+            exp_code = 32'hb;
+        end
+    end
 endmodule
